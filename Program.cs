9d9125a8862d5f3600a6950f787245e0937e7ba3
EbuElectrify <EbuElectrify@gmail.com>
2022-04-22 01:58:32 +0100

    PrimeNo-check

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+//Lab 3; write a c# program to check prime number
+//input 17
+//output: prime number
+//input 57
+//output:not prime number
+namespace PrimeNo
+{
+    class Program    {
+                static void Main(string[] args)    {
+            int first = 17;
+             Console.WriteLine("First number is 17");
+            //check divisibility
+            if (first % 2 != 0 && first % 3 != 0 && first % 5 != 0 && first % 7 != 0)        {
+                Console.WriteLine("This is a prime number");
+            }
+              else       {
+                Console.WriteLine("This is Not a Prime Number");
+            }
+            int second = 57;
+              Console.WriteLine("Second number is 57");
+            //check divisibility
+            if (second % 2 != 0 && second % 3 != 0 && second % 5 != 0 && second % 7 != 0)        {
+                Console.WriteLine("This is a prime number");
+            }
+              else       {
+                Console.WriteLine("This is Not a Prime Number");
+            }
+        }
+    }
+}
